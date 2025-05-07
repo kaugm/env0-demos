d64d0fdad1328f1060eb51335a4e302cdca6b5c6
Karl Martin <karlmartin895@gmail.com>
2025-05-07 10:06:46 -0400

    Update main.tf

--- cloud-to-code-demo/main.tf
+++ cloud-to-code-demo/main.tf
@@ -18,15 +18,6 @@ resource "aws_s3_bucket_ownership_controls" "example" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.public_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
 resource "aws_s3_bucket_policy" "public_read_policy" {
   bucket = aws_s3_bucket.public_bucket.id
 

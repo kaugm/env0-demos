29beb1b4df9327c10cbc2886d6b5f44111e42c9f
Karl Martin <karlmartin895@gmail.com>
2025-06-09 16:15:36 -0400

    fix

--- simple-serverless-stack/modules/handler/main.tf
+++ simple-serverless-stack/modules/handler/main.tf
@@ -1,10 +1,3 @@
-data "archive_file" "lambda_hello_world" {
-  type = "zip"
-
-  source_dir  = "${path.module}/hello-world"
-  output_path = "${path.module}/hello-world.zip"
-}
-
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = "kaugm-testing-bucket-sdfkj3248g"
 }
@@ -23,21 +16,9 @@ resource "aws_s3_bucket_acl" "lambda_bucket" {
   acl    = "private"
 }
 
-resource "aws_s3_object" "lambda_hello_world" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-
-  key    = "hello-world.zip"
-  source = data.archive_file.lambda_hello_world.output_path
-
-  etag = filemd5(data.archive_file.lambda_hello_world.output_path)
-}
-
 
 resource "aws_lambda_function" "hello_world" {
   function_name = "HelloWorld"
-
-  s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_object.lambda_hello_world.key
 
   runtime = "nodejs20.x"
   handler = "hello.handler"

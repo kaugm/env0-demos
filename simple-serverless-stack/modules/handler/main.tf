e6e3cea57bdd2920b7b24d8096f1dd6e0cb8e2eb
Karl Martin <karlmartin895@gmail.com>
2025-06-09 16:17:29 -0400

    fix

--- simple-serverless-stack/modules/handler/main.tf
+++ simple-serverless-stack/modules/handler/main.tf
@@ -23,8 +23,6 @@ resource "aws_lambda_function" "hello_world" {
   runtime = "nodejs20.x"
   handler = "hello.handler"
 
-  source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
-
   role = aws_iam_role.lambda_exec.arn
 
   # CLOUD TO CODE TESTING: MODIFY LAMBDA MEMORY_SIZE AND TIMEOUT ATTRIBUTES

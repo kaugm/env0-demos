bd94205c446088834fbe573980e3b2687dfed505
Karl Martin <karlmartin895@gmail.com>
2025-06-09 15:55:40 -0400

    naming fix

--- simple-serverless-stack/modules/handler/main.tf
+++ simple-serverless-stack/modules/handler/main.tf
@@ -5,6 +5,24 @@ data "archive_file" "lambda_hello_world" {
   output_path = "${path.module}/hello-world.zip"
 }
 
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = random_pet.lambda_bucket_name.id
+}
+
+resource "aws_s3_bucket_ownership_controls" "lambda_bucket" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "lambda_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.lambda_bucket]
+
+  bucket = aws_s3_bucket.lambda_bucket.id
+  acl    = "private"
+}
+
 resource "aws_s3_object" "lambda_hello_world" {
   bucket = aws_s3_bucket.lambda_bucket.id
 
--- simple-serverless-stack/outputs.tf
+++ simple-serverless-stack/outputs.tf
@@ -13,7 +13,7 @@ output "lambda_bucket_name" {
 output "function_name" {
   description = "Name of the Lambda function."
 
-  value = aws_lambda_function.hello_world.function_name
+  value = module.lambda_function.function_name
 }
 
 

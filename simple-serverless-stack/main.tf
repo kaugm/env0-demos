de93ff86fb983a63f8e125a1ea66e56178f422ee
Karl Martin <karlmartin895@gmail.com>
2025-06-09 16:29:26 -0400

    fix

--- simple-serverless-stack/main.tf
+++ simple-serverless-stack/main.tf
@@ -73,7 +73,7 @@ resource "aws_apigatewayv2_route" "hello_world" {
 }
 
 resource "aws_cloudwatch_log_group" "api_gw" {
-  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+  name = "/aws/api_gw/env0kaugmcloudtocodetesting"
 
   retention_in_days = 30
 }

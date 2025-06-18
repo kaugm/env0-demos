43191266c300b31836ff08a90b9a780e5d992c21
Karl Martin <karlmartin895@gmail.com>
2025-06-18 09:58:08 -0500

    Add files via upload

--- random/main.tf
+++ random/main.tf
@@ -1,10 +1,14 @@
 terraform {
   required_providers {
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.1.0"
     }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
 
 provider "random" {
@@ -12,20 +16,25 @@ provider "random" {
 }
 
 variable "refresh_date" {
-  type = string
+  type    = string
   default = "00/00/0000"
 }
 
 resource "random_string" "random" {
   keepers = {
-      refresh_date = var.refresh_date
+    refresh_date = var.refresh_date
   }
-  length           = 5
-  upper            = true
-  special          = false
+  length  = 5
+  upper   = true
+  special = false
 }
 
 output "random_string" {
-    value = random_string.random.result
+  value = random_string.random.result
 }
 
+
+import {
+  to = aws_s3_bucket.aws_s_3_bucket_wo5
+  id = "serverless-webapp-resources-env0-karl"
+}

329cb595eb7797d21f369359bcf10201e9aebfa5
Karl Martin <karlmartin895@gmail.com>
2025-07-08 16:31:37 -0400

    remove

--- terragrunt-ec2/modules/ec2-instance/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "example" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  count         = var.instance_count
-
-  tags = {
-    Name = var.name
-  }
-}
--- terragrunt/env0/bootstrap/provider.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_version = "~>1.4.0"
-  required_providers {
-    env0 = {
-      source = "env0/env0"
-    }
-  }
-}

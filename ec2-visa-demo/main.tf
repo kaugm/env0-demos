3186a9b7b37cd75d442a0fb99089ceb022bd787e
Karl Martin <karlmartin895@gmail.com>
2025-07-15 10:21:17 -0400

    visa demo

---
+++ ec2-visa-demo/main.tf
@@ -0,0 +1,85 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_iam_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Multiple AWS Managed Policies via for_each
+resource "aws_iam_role_policy_attachment" "ec2_policy_attachments" {
+  for_each   = toset(var.managed_policy_arns)
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = each.value
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_iam_role"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_security_group" "sg_test1" {
+  name        = "sg_test1"
+  description = "Security group for EC2 instance"
+  vpc_id      = var.vpc_id # You must provide this
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.10.10.10/32"]
+  }
+
+  # Continually Add Egress Rules
+  dynamic "egress" {
+    for_each = var.egress_rules
+    content {
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+    }
+  }
+}
+
+resource "aws_instance" "example" {
+  ami                    = data.aws_ami.ubuntu.id
+  subnet_ids = data.aws_subnet_ids.selected.ids
+  instance_type          = "t2.micro"
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  security_groups        = [aws_security_group.sg_test1.name]
+
+
+}
+
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+data "aws_vpc" "selected" {
+  id = var.vpc_id
+}
+
+data "aws_subnet_ids" "selected" {
+  vpc_id = data.aws_vpc.selected.id
+}
---
+++ ec2-visa-demo/variables.tf
@@ -0,0 +1,28 @@
+
+# Add more policies to list to be applied
+variable "managed_policy_arns" {
+  description = "List of AWS managed policy ARNs to attach to the EC2 IAM role"
+  type        = list(string)
+  default     = [
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  ]
+}
+
+# Add more egress rules to be applied
+variable "egress_rules" {
+  description = "List of egress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["10.10.10.11/32"]
+    }
+  ]
+}

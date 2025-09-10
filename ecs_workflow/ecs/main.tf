3adb33365282a1e7034b27f40bf0f3b6fdbdb639
Karl Martin <karlmartin895@gmail.com>
2025-09-10 12:38:56 -0400

    update

--- ecs_workflow/ecs/main.tf
+++ ecs_workflow/ecs/main.tf
@@ -25,8 +25,8 @@ resource "aws_ecs_cluster" "main" {
 
 
 # Output the DNS name of the cluster
-output "ecs_cluster_name" {
-  description = "The name of the ECS cluster"
-  value       = aws_ecs_cluster.main.name
+output "ecs_cluster_id" {
+  description = "The id of the ECS cluster"
+  value       = aws_ecs_cluster.main.id
 }
 

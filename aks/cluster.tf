e41078dc7dbd22bd85281d5a51f7ebf21f9d4fc7
Karl Martin <karlmartin895@gmail.com>
2025-09-12 17:19:46 -0400

    Update cluster.tf

--- aks/cluster.tf
+++ aks/cluster.tf
@@ -16,7 +16,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   network_profile {
     network_plugin     = "azure"
-    dns_service_ip     = "10.0.0.10"
     docker_bridge_cidr = "172.17.0.1/16"
   }
 }

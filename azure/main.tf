fd50b3f1a4ab1bfb4c7b3cbb4c47ba0305754fc2
Karl Martin <karlmartin895@gmail.com>
2025-09-12 13:14:20 -0400

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -1,5 +1,6 @@
 # Configure the Azure provider
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -98,10 +99,10 @@ resource "azurerm_managed_disk" "data_disk" {
 
 # 3. Attach the managed disk to the VM
 resource "azurerm_virtual_machine_data_disk_attachment" "data_disk_attachment" {
-  managed_disk_id      = azurerm_managed_disk.data_disk.id
-  virtual_machine_id   = azurerm_linux_virtual_machine.vm.id
-  lun                  = 10
-  caching              = "ReadWrite"
+  managed_disk_id    = azurerm_managed_disk.data_disk.id
+  virtual_machine_id = azurerm_linux_virtual_machine.vm.id
+  lun                = 10
+  caching            = "ReadWrite"
 }
 
 # Output the public IP address of the VM

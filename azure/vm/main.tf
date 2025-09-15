36182c33a0a82d6ff124de445d577893637600e4
Karl Martin <karlmartin895@gmail.com>
2025-09-15 15:24:51 -0400

    Update main.tf

--- azure/vm/main.tf
+++ azure/vm/main.tf
@@ -74,6 +74,36 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+
+
+# Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "Ansible-sg-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "ssh_rule" {
+  name                        = "allow_ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg_association" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+
+
 # 3. Create a virtual machine (VM)
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = var.vm_name

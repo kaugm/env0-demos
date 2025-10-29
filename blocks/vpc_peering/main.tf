e6395d367c7085b289eb61b3258a24a64768c813
Karl Martin <karlmartin895@gmail.com>
2025-10-29 15:05:17 -0400

    Update main.tf

--- blocks/vpc_peering/main.tf
+++ blocks/vpc_peering/main.tf
@@ -32,7 +32,7 @@ resource "aws_route" "requester_route" {
   # Change this to your specific route table ID if not using the main one
   route_table_id            = var.requester_vpc_id.main_route_table_id
   # Destination CIDR block of the Accepter VPC
-  destination_cidr_block    = var.accepter_vpc_id.cidr_block
+  destination_cidr_block    = "10.85.0.0/16"
   # The ID of the VPC peering connection
   vpc_peering_connection_id = aws_vpc_peering_connection.example.id
 }
@@ -41,7 +41,7 @@ resource "aws_route" "accepter_route" {
   # Change this to your specific route table ID if not using the main one
   route_table_id            = var.accepter_vpc_id.main_route_table_id
   # Destination CIDR block of the Requester VPC
-  destination_cidr_block    = var.requester_vpc_id.cidr_block
+  destination_cidr_block    = "10.84.0.0/16"
   # The ID of the VPC peering connection
   vpc_peering_connection_id = aws_vpc_peering_connection.example.id
 }
